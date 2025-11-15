1a4b6f206cc2485fbb7805b0e14971b42c9387c2
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-11-15 10:31:31 +0600

    react ecommerce project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "./pages/Home";
+export default function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-center text-red-500 mt-10 ">
+        Tailwind is Working! ⚡
+      </h1>
+      <Home />
+    </>
+  );
+}
