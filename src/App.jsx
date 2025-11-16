de3016aad95eb17adbb61076340ffc5f87b43652
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-11-16 11:11:52 +0600

    add cart page with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,24 @@ import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import Signup from "./pages/Register";
 import Cart from "./pages/Cart";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-      </MainLayout>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <MainLayout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </MainLayout>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { login } from './features/authSlice';
+import { login } from '../redux/features/authSlice';
 
 const Login = () => {
   const [formData, setFormData] = useState({ email: '', password: '' });
